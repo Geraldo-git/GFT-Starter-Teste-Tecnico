12f035e06503f4f30df679c4ce283c32ac77fd93
Geraldo-git <laboratorio.inicial@gmail.com>
2022-03-22 16:36:05 -0300

    Exercicio02

---
+++ src/exercicio02/Main.java
@@ -0,0 +1,10 @@
+package exercicio02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa("Maria", "Rua Abc", "4004-4004");
+
+        System.out.println(pessoa);
+    }
+}
